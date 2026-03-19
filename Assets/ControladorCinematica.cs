a7caa843b5d0398dfe76d01447865482ca9e0858
Iván de Castilla Guitián <121395173+SmartWasTaken@users.noreply.github.com>
2026-03-19 01:42:47 +0100

    grandes cambios
    Ahora el vídeo está subido en la nube y no gasta nada en el proyecto

--- Assets/ControladorCinematica.cs
+++ Assets/ControladorCinematica.cs
@@ -6,13 +6,25 @@ public class ControladorCinematica : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public string escenaVolver = "SampleScene";
+    public GameObject pantallaCarga;
 
     void Start()
     {
         if (videoPlayer != null)
         {
+            if (pantallaCarga != null) pantallaCarga.SetActive(true);
+
             videoPlayer.loopPointReached += AlTerminarVideo;
+            videoPlayer.prepareCompleted += AlEstarListo;
+
+            videoPlayer.Prepare();
         }
+    }
+
+    void AlEstarListo(VideoPlayer vp)
+    {
+        if (pantallaCarga != null) pantallaCarga.SetActive(false);
+        vp.Play();
     }
 
     void AlTerminarVideo(VideoPlayer vp)
